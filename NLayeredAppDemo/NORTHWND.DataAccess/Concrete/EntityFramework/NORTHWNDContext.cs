21cb5d4e4915b0a00d4e9f5bb65cda84e2a5fb0a
BetulBircan <bircan.betul_1996@hotmail.com>
2022-10-13 00:47:05 +0300

    entity framework için klasör oluşturuldu ve içerisine veri tabanına bağlanması için gerekli kodlar yazıldı

---
+++ NLayeredAppDemo/NORTHWND.DataAccess/Concrete/EntityFramework/NORTHWNDContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NORTHWND.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NORTHWND.DataAccess.Concrete.EntityFramework
+{
+    public class NORTHWNDContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\MSSQLLocalDB ;Database = NORTHWND;Trusted_Connection=True");
+        }
+    }
+
+}
