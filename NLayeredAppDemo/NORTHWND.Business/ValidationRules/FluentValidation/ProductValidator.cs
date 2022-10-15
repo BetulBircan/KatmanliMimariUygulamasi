39924008161473a2d444802da3004d9c20e44abd
BetulBircan <bircan.betul_1996@hotmail.com>
2022-10-16 00:24:26 +0300

    validationların uygulanması ve ürünlerin ekleme güncelleme işlemlerinde oluşan hataları uyarma için gerekli kodlar yazıldı

--- NLayeredAppDemo/NORTHWND.Business/ValidationRules/FluentValidation/ProductValidator.cs
+++ NLayeredAppDemo/NORTHWND.Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FluentValidation;
+using NORTHWND.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,24 @@ using System.Threading.Tasks;
 
 namespace NORTHWND.Business.ValidationRules.FluentValidation
 {
-    internal class ProductValidator
+    public class ProductValidator : AbstractValidator<Product>
     {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty().WithMessage("Ürün İsmi Boş Geçilemez!!");
+            RuleFor(p => p.CategoryId).NotEmpty().WithMessage("Kategori Boş Geçilemez!!");
+            RuleFor(p => p.UnitPrice).NotEmpty().WithMessage("Fiyat Bilgisi Boş Geçilemez!!");
+            RuleFor(p => p.QuantityPerUnit).NotEmpty().WithMessage("Birim Adedi Boş Geçilemez!!");
+
+            RuleFor(p => p.UnitPrice).GreaterThan(0).WithMessage("Ürün Fiyatı Sıfırdan Küçük Olamaz");
+            RuleFor(p => p.UnitsInStock).GreaterThanOrEqualTo((short)0).WithMessage("Stock Miktarı Sıfırdan Küçük Olamaz!!");
+            RuleFor(p => p.UnitPrice).GreaterThan(10).When(p => p.CategoryId == 2).WithMessage("Unit Price Değeri 10 Değerinden Büyük Olmalı!!");
+            RuleFor(p => p.ProductName).Must(StartWithA).WithMessage("Ürün Adı A ile başlamalı!!");
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
     }
 }
--- NLayeredAppDemo/NORTHWND.DataAccess/Concrete/EntityFramework/EfEntityRepositoryBase.cs
+++ NLayeredAppDemo/NORTHWND.DataAccess/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using NORTHWND.DataAccess.Abstract;
 using NORTHWND.Entities.Abstract;
 using System;
 using System.Collections.Generic;
