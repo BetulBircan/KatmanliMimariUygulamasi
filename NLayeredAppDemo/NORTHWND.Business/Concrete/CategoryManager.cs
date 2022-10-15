2f427ba8026eef2e68d7f2ed9d05d7553cfc1719
BetulBircan <bircan.betul_1996@hotmail.com>
2022-10-15 18:22:18 +0300

    ICategoryDal dan referans alarak tüm kategorileri listeleme işlemi burada yapıldı

---
+++ NLayeredAppDemo/NORTHWND.Business/Concrete/CategoryManager.cs
@@ -0,0 +1,26 @@
+﻿using NORTHWND.Business.Abstract;
+using NORTHWND.DataAccess.Abstract;
+using NORTHWND.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NORTHWND.Business.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+    }
+
+}
